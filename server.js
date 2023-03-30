cdce82e101278a82b1e2b3cf6f1ac9e86ea65d83
lexiisying <nyxxing0824@gmail.com>
2023-03-31 00:24:57 +0100

    practicing Query Parameters

--- server.js
+++ server.js
@@ -3,11 +3,25 @@ const app = express();
 const port = 3000;
 
 app.get("/", function (req, res) {
-  res.send("Yay Node!");
+    let searchQuery = req.query.search;
+  res.send("Yay Node!" + searchQuery);
 });
 
 app.get("/chocolate", function (req, res) {
-  res.send("Mm chocolate :O");
+    let searchQuery = req.query.amount;
+    res.send("Mm chocolate :O, you have " + searchQuery);
+});
+
+app.get("/json", function (req, res) {
+  let lat = req.query.lat;
+  let lng = req.query.lng;
+  res.send(`You searched for Lat: ${lat} and Lng: ${lng}`);
+});
+
+app.get("/multiply", function (req, res) {
+  let value1 = req.query.value1;
+  let value2 = req.query.value2;
+  res.send(`multiplied value is ${value1 * value2}`);
 });
 
 app.listen(3000, function () {
