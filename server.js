088dd4fb0dd05a11c621d102615616e34ccba4e6
lexiisying <nyxxing0824@gmail.com>
2023-03-31 00:05:13 +0100

    coursework

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-just testing 
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", function (req, res) {
+  res.send("Yay Node!");
+});
+
+app.get("/chocolate", function (req, res) {
+  res.send("Mm chocolate :O");
+});
+
+app.listen(3000, function () {
+  console.log("Server is listening on port 3000. Ready to accept requests!");
+});
